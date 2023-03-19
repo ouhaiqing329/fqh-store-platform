d51966da10e9ffcdb2419cd126876b047b85ab4a
ouhaiqing <ouhaiqing@tom.com>
2023-03-20 01:14:45 +0800

    增加Gateway网关断言请求转发

---
+++ gateway-server/src/main/java/com/fqh/gateway/GatewayServerApplication.java
@@ -0,0 +1,10 @@
+package com.fqh.gateway;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+}
