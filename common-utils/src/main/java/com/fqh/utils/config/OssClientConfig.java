bb5d8597716925ebb6be3a4721661cb7689c71a0
ouhaiqing <ouhaiqing@tom.com>
2024-07-06 19:57:53 +0800

    common init

--- common-utils/src/main/java/com/fqh/utils/config/OssClientConfig.java
+++ common-utils/src/main/java/com/fqh/utils/config/OssClientConfig.java
@@ -14,10 +14,10 @@ public class OssClientConfig {
     @Bean
     public OSS getOssCLient(){
         // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-        String endpoint = "https://bucket10002.oss-cn-shenzhen.aliyuncs.com/";
+        String endpoint = "";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tBk4nGkBMoFMtRv8itZ";
-        String accessKeySecret = "jrbPm9i2N0jlo6Q09Wtgkh2iB8HA7J";
+        String accessKeyId = "";
+        String accessKeySecret = "";
 
         // 创建ClientConfiguration。ClientConfiguration是OSSClient的配置类，可配置代理、连接超时、最大连接数等参数。
         ClientBuilderConfiguration conf = new ClientBuilderConfiguration();
